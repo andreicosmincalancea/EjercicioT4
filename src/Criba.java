c70f9887d7e856302615211692d515c106b03340
andreicosmincalancea <alucal6600@ieselcaminas.org>
2023-03-02 21:03:39 +0100

    Programa original

--- src/Criba.java
+++ src/Criba.java
@@ -1,90 +1,60 @@
 import java.util.Scanner;
-/**
- * Clase Criba:
- * Dado un número, imprimer dos arrays. El primero con todos los enteros y el segundo únicamente
- * los primos hasta el número especificado
- * @author Andrei Cosmin Calancea
- * @version 1.2
- */
-public class Criba {
-    /**
-     * Función que genera los números primos desde el 2 hasta el número máximo especificado
-     * @return un array de integers primos
-     * @param numeroMaximo último número al que llegar para determinar los números primos
-     */
-    public static int[] generarPrimos(int numeroMaximo) {
-
-        int i;
-        int j;
-        int tamanyo = numeroMaximo + 1;
-        int cuenta = 0;
-        boolean[] esPrimo = new boolean[tamanyo];
-
-        if (numeroMaximo >= 2) {
-            for (i = 0; i < tamanyo; i++) {
+public class Criba
+{
+    // Generar números primos de 1 a max
+    public static int[] generarPrimos (int max)
+    {
+        int i,j;
+        if (max >= 2) {
+// Declaraciones
+            int dim = max + 1; // Tamaño del array
+            boolean[] esPrimo = new boolean[dim];
+// Inicializar el array
+            for (i=0; i<dim; i++)
                 esPrimo[i] = true;
-            }
-
-            esPrimo[0] = false;
-            esPrimo[1] = false;
-
-            for (i = 2; i < Math.sqrt(tamanyo) + 1; i++) {
+// Eliminar el 0 y el 1, que no son primos
+            esPrimo[0] = esPrimo[1] = false;
+// Criba
+            for (i=2; i<Math.sqrt(dim)+1; i++) {
                 if (esPrimo[i]) {
-                    for (j = 2 * i; j < tamanyo; j += i) {
+// Eliminar los múltiplos de i
+                    for (j=2*i; j<dim; j+=i)
                         esPrimo[j] = false;
-                    }
                 }
             }
-
-            for (i = 0; i < tamanyo; i++) {
-                if (esPrimo[i]) {
+// ¿Cuántos primos hay?
+            int cuenta = 0;
+            for (i=0; i<dim; i++) {
+                if (esPrimo[i])
                     cuenta++;
-                }
             }
-
+// Rellenar el vector de números primos
             int[] primos = new int[cuenta];
-
-            for (i = 0, j = 0; i < tamanyo; i++) {
-                if (esPrimo[i]) {
+            for (i=0, j=0; i<dim; i++) {
+                if (esPrimo[i])
                     primos[j++] = i;
-                }
             }
-
             return primos;
-
-        } else {
+        } else { // max < 2
             return new int[0];
+// Vector vacío
         }
     }
-    /**
-     * Función que solicita un número, imprime los enteros hasta ese número y los primos hasta el
-     * mismo número
-     */
     public static void main(String[] args) {
-
-        Scanner teclado = new Scanner(System.in);
+        Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
-
-        int dato = teclado.nextInt();
-        int[] vector = new int[dato];
-
+        int dato=teclado.nextInt();
+        int vector[]=new int[dato];
         System.out.println("\nVector inicial hasta :"+dato);
-
         for (int i = 0; i < vector.length; i++) {
-            if (i % 10 == 0) {
-                System.out.println();
-            }
-            System.out.print(i + 1 + "\t");
+            if (i%10==0) System.out.println();
+            System.out.print(i+1+"\t");
         }
-        // Determinamos los números primos
-        vector = generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:" + dato);
-
+        vector=generarPrimos(dato);
+        System.out.println("\nVector de primos hasta:"+dato);
         for (int i = 0; i < vector.length; i++) {
-            if (i % 10 == 0) {
-                System.out.println();
-            }
-            System.out.print(vector[i] + "\t");
+            if (i%10==0) System.out.println();
+            System.out.print(vector[i]+"\t");
         }
     }
 }
